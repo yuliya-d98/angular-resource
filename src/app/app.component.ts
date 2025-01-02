6fe3986b3463c09419f6ff8c84aa4b22d572e7a2
Yuliya Dubovik <feelgloom@gmail.com>
2025-01-02 12:01:23 +0300

    [feat] users list, update, add, clear users, update users list resource on query signal change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {UserSearchComponent} from './user-search/user-search.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserSearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/user-search/user-search.component.ts
@@ -0,0 +1,68 @@
+import {Component, effect, resource, signal, ResourceStatus, computed} from '@angular/core';
+import {API_URL} from './config';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {User} from './model';
+
+type TRequest = {
+  query: string
+}
+
+@Component({
+  selector: 'app-user-search',
+  imports: [MatProgressBarModule],
+  templateUrl: './user-search.component.html',
+  styleUrl: './user-search.component.scss',
+  standalone: true
+})
+export class UserSearchComponent {
+  query = signal('');
+  users = resource<User[], TRequest>({
+    // loader: () => {
+    //   return fetch(`${API_URL}`).then((data) => data.json())
+    // },
+    loader: async ({request, abortSignal}) => {
+      // request содержит в себе то, что мы вернули в текущем ресурсе в поле request
+      // const users = await fetch(`${API_URL}?name_like=^${request.query}`, {
+      //   signal: abortSignal
+      // });
+      // можем сделать кастомный аборт сигнал: (slow 3G)
+      const customAbortCtrl = new AbortController();
+      setTimeout(() => customAbortCtrl.abort(`Too long response...`), 500);
+      const users = await fetch(`${API_URL}?name_like=^${request.query}`, {
+        signal: customAbortCtrl.signal
+      });
+      //
+      // если в fetch приходит 4** или 5** статус код,
+      // он не считает это ошибкой и в resource
+      // не прокидывается  users.errors(), поэтому сделаем это сами
+      if (!users.ok) throw Error(`Could not fetch...`)
+      return users.json();
+    },
+    request: (): TRequest => {
+      // при изменении сигнала this.query()
+      // будет вызываться loader для ресурса
+      // users и будут обновляться данные
+      return {query: this.query()}
+      // можно вернуть просто сигнал, можно объект с сигналами
+    }
+  });
+  // для загрузки такое реализовывать не нужно,
+  // уже есть в самом resource: this.users.isLoading()
+  // isLoading = computed(() => {
+  //   return this.users.status() === ResourceStatus.Loading
+  //     || this.users.status() === ResourceStatus.Reloading
+  // })
+
+  constructor() {
+    effect(() => {
+      console.log(`Status: ${this.users.status()}`);
+      // console.log('Enum with statuses from Angular: ', ResourceStatus)
+    })
+  }
+
+  addUser() {
+    const userId = this.users.value()?.length ? 123 + this.users.value()!.length : 123;
+    const user: User = { id: userId, name: "Dmytro Mezhenskyi" };
+    this.users.update((users) => users ? [user, ...users] : [user])
+  }
+}
